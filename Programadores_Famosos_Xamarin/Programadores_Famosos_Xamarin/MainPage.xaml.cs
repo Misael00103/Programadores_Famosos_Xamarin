8eea563fc0ed3c6b424357629c74d9b4568d0eaa
Misael00103 <beriguetemisael@gmail.com>
2022-08-21 21:40:34 -0400

    first commit

---
+++ Programadores_Famosos_Xamarin/Programadores_Famosos_Xamarin/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+
+namespace Programadores_Famosos_Xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLeerMas_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/Guido_van_Rossum", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnLeerMas1_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/Brendan_Eich", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnLeerMas2_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/Rasmus_Lerdorf", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnLeerMas3_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/Matt_Mullenweg", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnLeerMas4_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/Tim_Berners-Lee", BrowserLaunchMode.SystemPreferred);
+        }
+    }
+}
+
